ed6f55db65c658956eff065eb707ab057b5fffa3
unknown <italokornejo@gmail.com>
2022-12-02 19:04:43 -0500

    feat: new port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./utils/database')
 const app = express();
-const port = 9000;
+const port = 9001;
 
 const userRouter = require('./users/users.router')
 
